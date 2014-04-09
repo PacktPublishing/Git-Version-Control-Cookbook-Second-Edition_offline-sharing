2a6f0eb9e9a9bb339aa00daa3ecd05c9a730a28d
John Doe <john.doe@example.com>
2014-04-09 21:28:51 +0200

    Adds checker for prime number

--- math.c
+++ math.c
@@ -3,6 +3,7 @@
  *
  */
 #include <stdio.h>
+#include <math.h>
 
 /* Short pi calculation */
 void pi(void){
@@ -11,10 +12,34 @@ void pi(void){
     printf("%f\n",x);
 }
 
+/* More funny math
+   Check if number is a prime no
+*/
+int is_prime(int n){
+    if (n<2){
+        return 0;
+    }
+    if (n == 2) {
+        return 1;
+    }
+    int max = ceil(sqrt(n));
+    int i = 2;
+    while (i <= max) {
+        if (n % i == 0){
+            printf("%d is not a prime number\n", n);
+            return 0;
+        }
+        i++;
+    }
+    printf("%d is a prime number!\n", n);
 
+    return 0;
+}
 
 int main (void){
     pi();
+
+    is_prime(223);
 
     return 0;
 }
